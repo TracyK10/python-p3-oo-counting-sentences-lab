51b3562d8783b39b662f54e25348d70a134644e6
Tracy Karanja <chirutracy@gmail.com>
2024-05-26 17:21:31 +0300

    new commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.value = value
+    
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, word):
+    if(type(word) == str):
+      self._value = word
+    else:
+      print("The value must be a string.")
+  
+  def is_sentence(self):
+    return self._value.endswith(".")
+    
+  def is_question(self):
+    return self._value.endswith("?")
+  
+  def is_exclamation(self):
+    return self._value.endswith("!")
+    
+  def count_sentences(self):
+    # count = 0
+    # for char in self._value:
+    #   if char == "." or char == "?" or char == "!":
+    #     count += 1
+    # return count
+    value = self._value
+    for punctuation in ['!', "?", '.']:
+      value = value.replace(punctuation, '.')
+    sentences = filter(None, value.split('.'))
+    return len(list(sentences))
+
+#examples
+string = MyString()
+string.value = "This is a string! It has three sentences. Right?"
+print(string.count_sentences())
